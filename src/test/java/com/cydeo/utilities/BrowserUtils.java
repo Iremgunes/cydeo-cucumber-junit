807eb3dab8425c1521b2ad5c49c844ad057d0d34
Iremgunes <aviremdanacioglu@gmail.com>
2022-06-16 16:08:46 +0300

    Initial commit

---
+++ src/test/java/com/cydeo/utilities/BrowserUtils.java
@@ -0,0 +1,63 @@
+package com.cydeo.utilities;
+
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+public class BrowserUtils {
+
+    public static void sleep(int second){
+        second*=1000;
+        try {
+            Thread.sleep(second);
+        }catch (InterruptedException e){
+
+        }
+    }
+
+    /*
+    this method accepts 3 arguments
+    arg1: webdriver
+    arg2 : expectedInUrl: for verify if the url contains given string
+        if condition matches will break loop
+    arg3: expectedInTitle to be compared against actualTitle
+     */
+
+    public static void switchWindowAndVerify( String expectedInUrl, String expectedInTitle){
+
+        Set<String> allWindowsHandles = Driver.getDriver().getWindowHandles();
+
+        for (String each : allWindowsHandles) {
+
+            Driver.getDriver().switchTo().window(each);
+
+            System.out.println("current url :" + Driver.getDriver().getCurrentUrl());
+
+            if(Driver.getDriver().getCurrentUrl().contains(expectedInUrl)){
+                break;
+            }
+
+
+        }
+
+        String actualTitle = Driver.getDriver().getTitle();
+
+        Assert.assertTrue(actualTitle.contains(expectedInTitle));
+
+    }
+
+    public static void verifyTitle( String expectedTitle){
+        Assert.assertEquals(Driver.getDriver().getTitle(), expectedTitle);
+    }
+
+    public static void waitForInvisibilityOf(WebElement webElement){
+        Driver.getDriver().manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10);
+        wait.until(ExpectedConditions.invisibilityOf(webElement));
+
+    }
+}
