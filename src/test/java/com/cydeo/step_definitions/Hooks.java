9af7ace6ce5ab50d2966cd54a7da587c6628b000
Iremgunes <aviremdanacioglu@gmail.com>
2022-06-18 13:00:13 +0300

    18 jun commit 3

--- src/test/java/com/cydeo/step_definitions/Hooks.java
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -1,6 +1,22 @@
 package com.cydeo.step_definitions;
 
 
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
 public class Hooks {
+
+    //import from io.cucumber.java
+    @Before
+    public void setupScenario(){
+        System.out.println("=====Setting up browser using cucumber @Before");
+    }
+
+    @After
+    public void teardownScenario(){
+        System.out.println("====Closing browser using cucumber @After");
+    }
+
+
 
 }
